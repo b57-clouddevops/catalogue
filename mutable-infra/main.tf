5e44189901652a05fb6b873f3f24b4b0e8e81faf
CodingManoj <CodingManoj@gmail.com>
2024-05-03 21:34:49 -0400

    Auto Push

--- mutable-infra/main.tf
+++ mutable-infra/main.tf
@@ -3,6 +3,9 @@ module "catalogue" {
     ENV                     = var.ENV
     INTERNAL                = var.INTERNAL 
     COMPONENT               = var.COMPONENT
+    APP_PORT                = var.APP_PORT
+    SPOT_INSTANCE_COUNT     = var.SPOT_INSTANCE_COUNT
+    SPOT_INSTANCE_TYPE      = var.SPOT_INSTANCE_TYPE
+    OD_INSTANCE_COUNT       = var.OD_INSTANCE_COUNT
+    OD_INSTANCE_TYPE        = var.OD_INSTANCE_TYPE
 }
-
-
